2d10e670f40c0c0e1157d83bf601ba7394749628
Giant_Salted_Fish <wangzhichuan6@outlook.com>
2023-10-30 03:00:41 +0800

    Add PatchedKeyBinding.Builder.

--- src/main/java/gsf/kbp/client/api/PatchedKeyBinding.java
+++ src/main/java/gsf/kbp/client/api/PatchedKeyBinding.java
@@ -2,9 +2,17 @@ package gsf.kbp.client.api;
 
 import gsf.kbp.client.IKeyBinding;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.client.util.InputMappings.Input;
+import net.minecraft.client.util.InputMappings.Type;
 import net.minecraftforge.client.settings.IKeyConflictContext;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -27,5 +35,87 @@ public class PatchedKeyBinding extends KeyBinding implements IPatchedKeyBinding
 		
 		final IKeyBinding ikb = ( IKeyBinding ) this;
 		ikb._setDefaultCombinations( combinations );
+	}
+	
+	public static class Builder
+	{
+		protected final String description;
+		
+		protected String category = "key.categories.gameplay";
+		
+		protected Input key = InputMappings.UNKNOWN;
+		
+		protected Set< Input > combinations = Collections.emptySet();
+		
+		protected IKeyConflictContext conflict_context = KeyConflictContext.IN_GAME;
+		
+		public Builder( String description ) {
+			this.description = description;
+		}
+		
+		public Builder withCategory( String category )
+		{
+			this.category = category;
+			return this;
+		}
+		
+		public Builder withKey( Input key )
+		{
+			this.key = key;
+			return this;
+		}
+		
+		public Builder withKeyboardKey( int key_code )
+		{
+			this.key = Type.KEYSYM.getOrCreate( key_code );
+			return this;
+		}
+		
+		public Builder withMouseButton( int button )
+		{
+			this.key = Type.MOUSE.getOrCreate( button );
+			return this;
+		}
+		
+		public Builder withCombinations( Input... combinations )
+		{
+			this.combinations = new HashSet<>( Arrays.asList( combinations ) );
+			return this;
+		}
+		
+		public Builder withKeyboardCombinations( int... combinations )
+		{
+			this.combinations = new HashSet<>();
+			for ( int code : combinations )
+			{
+				final Input input = Type.KEYSYM.getOrCreate( code );
+				this.combinations.add( input );
+			}
+			return this;
+		}
+		
+		public Builder withConflictContext( IKeyConflictContext context )
+		{
+			this.conflict_context = context;
+			return this;
+		}
+		
+		public PatchedKeyBinding build()
+		{
+			return new PatchedKeyBinding(
+				this.description,
+				this.conflict_context,
+				this.key,
+				this.combinations,
+				this.category
+			);
+		}
+		
+		public PatchedKeyBinding buildAndRegis()
+		{
+			final PatchedKeyBinding kb = this.build();
+			ClientRegistry.registerKeyBinding( kb );
+			return kb;
+		}
 	}
 }
